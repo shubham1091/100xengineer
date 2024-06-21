130b91cc2d604258d2da0767867528c45f075500
shubham <shubhamverma1091@gmail.com>
2024-06-21 17:25:57 +0530

    blogging project init

---
+++ week-7/blog/src/App.jsx
@@ -0,0 +1,11 @@
+import { config } from "./config";
+function App() {
+  console.log(config);
+  return (
+    <>
+      <h1 className="bg-red-400">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
