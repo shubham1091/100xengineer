bdc7dad2e9591ea0f75fc3b414191b21fe2459d3
shubham <shubhamverma1091@gmail.com>
2024-09-29 11:52:09 +0530

    file handling setup done

--- ClipCast/backend/src/env.ts
+++ ClipCast/backend/src/env.ts
@@ -7,5 +7,8 @@ declare module "bun" {
     REFRESH_TOKEN_SECRET: string;
     ACCESS_TOKEN_EXPIRY: string;
     REFRESH_TOKEN_EXPIRY: string;
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
   }
 }
