7a01acc115313d0e147670d3474a4b9f410d9e87
shubham <shubhamverma1091@gmail.com>
2024-10-11 07:59:36 +0530

    added shadcn

---
+++ week-12/segment_slip/src/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { useAuthStore } from "@/store/Auth";
+import { useRouter } from "next/router";
+import { ReactNode, useEffect } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  const { session } = useAuthStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      router.push("/");
+    }
+  }, [session, router]);
+
+  if (session) {
+    return null;
+  }
+
+  return (
+    <div className="">
+      <div className="">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
